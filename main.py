449e59d0e535bd5c8cfcb0090caf7b71131fefb5
Philip Sinnott <philipsinnotts@gmail.com>
2022-06-07 22:14:19 +0200

    Simplified code and added working encrypt function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from shift_cipher import ShiftCipher
 def main():
     SHIFT = ShiftCipher()
-    print(SHIFT.alphabet_pos("khan"))
+    # print(SHIFT.alphabet_pos("khan"))
+    print(SHIFT.encrypt("khan"))
+
+    # print(SHIFT.alphabet_to_numerical("khan"))
+    # SHIFT.numerical_to_alphabet([10, 7, 0, 13])
 
 if __name__ == "__main__":
     main()
--- shift_cipher.py
+++ shift_cipher.py
@@ -1,16 +1,30 @@
 from string import ascii_lowercase as ALPHABET
-LETTERS = {letter: index for index, letter in enumerate(ALPHABET)}
+KEY = 19
 
 class ShiftCipher:
 
-    def alphabet_pos(self, message):
-        message = message.lower()
+    def alphabet_to_numerical(self, message):
+        numerical = []
+        for character in message:
+            numerical.append(ALPHABET.find(character))
+        return numerical
 
-        numbers = [LETTERS[character] for character in message if character in LETTERS]
-        return numbers
+    def numerical_to_alphabet(self, message):
+        alphabet = []
+        for letter in message:
+            alphabet.append(chr(97 + letter))
+        return alphabet
 
     def encrypt(self, message):
-        pass
+        message = self.alphabet_to_numerical(list(message))
+        reverse = []
+        for character in message:
+            y = (character + KEY) % 26
+            reverse.append(y)
+
+        str_result = ""
+        reverse = self.numerical_to_alphabet(reverse)
+        return str_result.join(reverse)
 
     def decrypt(self, message):
         pass
